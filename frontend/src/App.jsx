75ff3a12f4aa2e2915285bde750b1a7561e5ff60
SwastikKaurav <kauravswastik15@gmail.com>
2026-04-06 19:32:32 +0530

    initial setup: added react frontend to existing project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/login" element={<h1>Login Page</h1>} />
+        <Route path="/register" element={<h1>Register Page</h1>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
