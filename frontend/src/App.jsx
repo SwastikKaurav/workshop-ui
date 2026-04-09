646a240da7a39660585520baac2595f128d7e5f3
SwastikKaurav <kauravswastik15@gmail.com>
2026-04-09 16:06:47 +0530

    add Login page with useState

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
+import Login from './pages/Login'
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<h1>Login Page</h1>} />
+        <Route path="/login" element={<Login />} />
         <Route path="/register" element={<h1>Register Page</h1>} />
       </Routes>
       <Footer />
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <div>
+        <label>Email</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter your email"
+        />
+      </div>
+
+      <div>
+        <label>Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Enter your password"
+        />
+      </div>
+
+      <button>Login</button>
+
+      <p>Don't have an account? <Link to="/register">Register</Link></p>
+    </div>
+  )
+}
+
+export default Login
